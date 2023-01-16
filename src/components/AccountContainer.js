470b25c4e886e84551088077a489f35a0d520b78
glen <117769783+Glen-ae@users.noreply.github.com>
2023-01-16 14:04:25 +0300

    Add files via upload

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,24 +1,37 @@
-import React, {useState, useEffect} from "react";
+import React, { useEffect, useState} from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
-  const [transactions, setTranscation] = useState([])
+  //store fetched data from side effect
+  const [transactions, setTransactions] = useState([]);
+
+  //manage state for user serch parameters
+  const [ search, setSearch] = useState(""); 
+  
+   //Featch all transaction from server
   useEffect(() => {
     fetch("http://localhost:8001/transactions")
-    .then(r =>r.json())
-    .then( (data) => setTranscation(data))
+      .then((r) => r.json())
+      .then((data) => setTransactions(data)) //Setter activity for transaction after server response
   }, [])
 
+  //Re-renders page with updated state after creating a new transaction
+  function updatedTransactions(newTransactions) {
+   const updatedTransactionsArray = [...transactions, newTransactions]
+   setTransactions(updatedTransactionsArray)
+  }
 
+  //render all components
   return (
     <div>
-      <Search />
-      <AddTransactionForm />
-      <TransactionsList arrayOfAllTransactions={transactions} />
+      <Search search={search} setSearch={setSearch}/>
+      <AddTransactionForm newData={updatedTransactions} />
+      <TransactionsList transactions={transactions} setTransactions={setTransactions} search={search} />
     </div>
   );
 }
+
 
 export default AccountContainer;
